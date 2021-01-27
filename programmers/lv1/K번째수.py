aed48c12fb98ff983c375250c8f30af1eeaec0e5
dohyunkim <kdh951102@gmail.com>
2021-01-27 16:48:02 +0900

    2021-1-27

--- programmers/lv1/K번째수.py
+++ programmers/lv1/K번째수.py
@@ -1,2 +1,29 @@
-dfsdf
-:w:
+array = list(map(int, input("array입력: ").split()))
+
+commands = []
+length = int(input("commands의 길이 입력: "))
+for i in range(length):
+    commands.append([])
+    print(i, end='')
+    commands[i] = list(map(int,input("번째 줄 입력: ").split()))
+
+print(commands)
+
+
+def solution(array, commands):
+    ans = []
+    length= len(commands)
+
+    for i in range(length):
+        new_array = []
+        start_index = commands[i][0]
+        end_index = commands[i][1]
+        extract_index = commands[i][2]
+        new_array = array[start_index-1:end_index]
+        new_array.sort()
+        ans.append(new_array[extract_index-1])
+    return ans
+
+ans = []
+ans = solution(array, commands)
+print(ans)
