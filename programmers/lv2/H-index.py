e64bdbcea42bf28e310985af89e4dcff2e28806d
dohyunkim <kdh951102@gmail.com>
2021-01-29 00:21:42 +0900

    정렬

--- programmers/lv2/H-index.py
+++ programmers/lv2/H-index.py
@@ -1,20 +1,23 @@
 
 def solution(citations):
-    l = citations
-    length = len(l)
+    ans = 0
     j = 0
-    while True:
+    for k in range(max(citations)):
         cnt = 0
-        for i in l:
+        for i in citations:
             if i >= j:
                 cnt += 1
 
         if j == cnt :
             ans = j
             break
+        elif j <= cnt:
+            ans = j
         j += 1
 
     return ans
 
-
-print(solution([3, 0, 6, 1, 5]))
+print(solution([10,9,4,1,1]))
+print(solution([0,1,3,5,5,5,5,5,5,6]))
+print(solution([0,1,1,1,1,3,3,4]))
+print(solution([0,0,0,0]))
