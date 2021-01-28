ea816a06dc866f4c64b257e826aef8579607e185
dohyunkim <kdh951102@gmail.com>
2021-01-28 17:00:41 +0900

    시간초과

--- programmers/lv2/가장_큰_수.py
+++ programmers/lv2/가장_큰_수.py
@@ -2,33 +2,35 @@ numbers = []
 numbers = list(map(int,input().split()))
 
 def solution(numbers):
-    numbers = list(map(str, numbers))
+    stack = []
+    tmp = []
     length = len(numbers)
 
-    stack = []
+    numbers=  list(map(str, numbers))
+
+    for index, number in enumerate(numbers):
+        tmp.append((number * 4)[:4])
+
+    tmp = list(map(int,tmp))
+
     while True:
-        max_elem = numbers[0]
-        for e in numbers:
-            if e[0] > max_elem[0]:
-                max_elem = e
-            elif e[0] == max_elem[0]:
-                if len(e) > len(max_elem) and e[-1] != '0':
-                    max_elem = e
-                elif len(e) == len(max_elem):
-                    for i in range(len(max_elem)):
-                        if e[i-1] > max_elem[i-1]:
-                            max_elem = e
+        max_ = tmp[0]
+        for e in tmp:
+            if e > max_:
+                max_ = e
 
-        stack.append(max_elem)
-        del numbers[numbers.index(max_elem)]
+        pop_i = tmp.index(max_)
+        stack.append(numbers[pop_i])
+        del numbers[pop_i]
+        del tmp[pop_i]
 
-        if len(stack) == length:
-            break
+        if len(stack) == length: break
 
-    print(stack)
-
-    return stack
+    stack = ''.join(stack)
+    return str(0) if not int(stack) else stack
 
 
 ans = solution(numbers)
 print(ans)
+
+
