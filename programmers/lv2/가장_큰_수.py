d44d43d056c52d75ad3b7fc2a058dee77913c994
dohyunkim <kdh951102@gmail.com>
2021-01-29 16:15:17 +0900

    가장 큰 수 성공

--- programmers/lv2/가장_큰_수.py
+++ programmers/lv2/가장_큰_수.py
@@ -3,30 +3,15 @@ numbers = list(map(int,input().split()))
 
 
 def solution(numbers):
+    numbers = list(map(str, numbers))
     stack = []
     tmp = []
-    length = len(numbers)
+    for number in numbers:
+        tmp.append([(number*4)[:4], number])
 
-    numbers=  list(map(str, numbers))
-
-    for index, number in enumerate(numbers):
-        tmp.append((number * 4)[:4])
-
-    tmp = list(map(int,tmp))
-
-    while True:
-        max_ = tmp[0]
-        for e in tmp:
-            if e > max_:
-                max_ = e
-
-        pop_i = tmp.index(max_)
-        stack.append(numbers[pop_i])
-        del numbers[pop_i]
-        del tmp[pop_i]
-
-        if len(stack) == length: break
-
+    tmp.sort(reverse=True)
+    for i in tmp:
+        stack.append(i.pop())
     stack = ''.join(stack)
     return str(0) if not int(stack) else stack
 
